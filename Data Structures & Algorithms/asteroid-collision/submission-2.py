5200219f9ca581db0eba2398a316768f48615e81
Anton Ilic <93786227+anton-ilic@users.noreply.github.com>
2026-09-24 09:26:33 -0400

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        ans = []
+
+        for asteroid in asteroids:
+            if len(ans) != 0 and ans[-1] > 0 and asteroid < 0:
+                # right moving ans, left moving asteroid
+
+                # shouldnt ans[-1] > 0 handle the case where its -2, -2, 1, -2
+                while len(ans) != 0 and ans[-1] > 0:
+                    if abs(asteroid) == abs(ans[-1]):
+                        ans.pop()
+                        asteroid = 0
+                        break
+                    elif abs(asteroid) > abs(ans[-1]):
+                        # asteroid is bigger
+                        ans.pop()
+                        
+                    else:
+                        # asteroid is smaller
+                        break
+                
+                if (len(ans) == 0 and asteroid != 0) or (asteroid > 0 and ans[-1] > 0) or (asteroid < 0 and ans[-1] < 0):
+                    ans.append(asteroid)
+
+            else:
+                ans.append(asteroid)
+
+
+        return ans
